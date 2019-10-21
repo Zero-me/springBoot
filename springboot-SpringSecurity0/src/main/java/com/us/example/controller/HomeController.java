9a81af8025a2844280a869844429b123d41de6b9
zero <475811868@qq.com>
2019-10-21 22:52:56 +0800

    修改文件使得项目正常运行

--- springboot-SpringSecurity0/src/main/java/com/us/example/controller/HomeController.java
+++ springboot-SpringSecurity0/src/main/java/com/us/example/controller/HomeController.java
@@ -15,6 +15,7 @@ public class HomeController {
     public String index(Model model){
         Msg msg =  new Msg("测试标题","测试内容","额外信息，只对管理员显示");
         model.addAttribute("msg", msg);
+        System.out.println("主页");
         return "home";
     }
 }
